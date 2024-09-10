ca213aefcd102d03fa36d415616f34e0e227a371
unknown <21.erdei.gergo.janos@mechwart.com>
2024-09-10 13:28:15 +0200

    Beolvasas

---
+++ Feladat/Foci/Foci/Feladatok.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foci
+{
+    internal class Feladatok
+    {            
+        public List<Meccs> meccsek;
+        public Feladatok(string filename)
+        {
+            foreach (var item in File.ReadAllLines(filename, Encoding.UTF8))
+            {
+                string[] reszek = item.Split(" ");
+                int fordulo = Convert.ToInt32(reszek[0]);
+                int hazaiVegeredmeny = Convert.ToInt32(reszek[1]);
+                int vendegVegeredmeny = Convert.ToInt32(reszek[2]);
+                int hazaiFelido = Convert.ToInt32(reszek[3]);
+                int vendegFelido = Convert.ToInt32(reszek[4]);
+                string hazaiCsapat = reszek[5];
+                string vendegCsapat = reszek[6];
+                Meccs uj = new Meccs(fordulo, hazaiVegeredmeny, vendegVegeredmeny, hazaiFelido, vendegFelido, hazaiCsapat, vendegCsapat);
+                meccsek.Add(uj);
+            }
+        } 
+        //
+    }
+}
---
+++ Feladat/Foci/Foci/Meccs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foci
+{
+    internal class Meccs
+    {
+        public int Fordulo { get; set; }
+        public int HazaiVegeredmeny { get; set; }
+        public int VendegVegeredmeny { get; set; }
+        public int HazaiFelido { get; set; }
+        public int VendegFelido { get; set; }
+        public string HazaiCsapat { get; set; }
+        public string VendegCsapat { get; set; }
+
+        public Meccs(int fordulo, int hazaiVegeredmeny, int vendegVegeredmeny, int hazaiFelido, int vendegFelido, string hazaiCsapat, string vendegCsapat)
+        {
+            Fordulo = fordulo;
+            HazaiVegeredmeny = hazaiVegeredmeny;
+            VendegVegeredmeny = vendegVegeredmeny;
+            HazaiFelido = hazaiFelido;
+            VendegFelido = vendegFelido;
+            HazaiCsapat = hazaiCsapat;
+            VendegCsapat = vendegCsapat;
+        }
+    }
+}
